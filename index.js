a8f26678bfb675d5f63610fd077914b8d3e37ce6
jihad-hossain1 <jihadkhan934@gmail.com>
2023-12-03 18:14:49 +0600

    some change with when deploy data are not fatch

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    await client.connect();
+    // await client.connect();
     const db = await client.db("kanbanTask");
     const tasksCollection = db.collection("tasks");
 
